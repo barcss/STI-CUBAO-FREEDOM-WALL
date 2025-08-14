1c8b23a3217fa874c381d17e696746bdcbe62978
John Paul Barcinilla <barcinillajohnpaul106@gmail.com>
2025-08-14 22:51:49 +0800

    Include script for dynamic content. Remove static sample posts

--- User/StudentArtGallery.php
+++ User/StudentArtGallery.php
@@ -1,16 +1,18 @@
-<?php 
+<?php
 include('../User/Components/UserMetaData.php');
 
 ?>
+
 <head>
     <style>
         .img-preview {
-        object-fit: cover;
-        object-position: center center;   /* Spacing from text */
-    }
-
+            object-fit: cover;
+            object-position: center center;
+            /* Spacing from text */
+        }
     </style>
 </head>
+
 <body class="poppins-regular">
     <div class="container-fluid vh-100 d-flex p-0 m-0 row">
 
@@ -22,7 +24,7 @@ include('../User/Components/UserMetaData.php');
             <div class="p-1 ">
                 <button id="userDashboard" class="w-100 btn text-white text-start p-1 primary-color"><i class="bi bi-person-circle mx-2 text-white"></i>User Dashboard</button>
             </div>
-        
+
             <!-- <div class=" bg-light overflow-hidden shadow-sm rounded d-flex flex-column align-items-center mt-3">
                 <div id="filter_btn_container" class="p-0 w-100 primary-fs">
                     <button value="user_management" class="p-2 bg-white w-100 rounded border text-start"><span class="primary-color p-1 me-2"></span>User Management</button>
@@ -37,7 +39,7 @@ include('../User/Components/UserMetaData.php');
             </div>
         </div>
 
-        <div class="col p-0 bg-light p-3 d-flex container m-0 row align-items-center flex-column overflow-scroll vh-100" id="content_container">
+        <div class="col-lg-6 col-12 bg-light p-3 d-flex  m-0 align-items-center flex-column overflow-scroll vh-100" id="content_container">
             <div class="bg-white     rounded-2 shadow-sm border primary-fs col-12 col-sm-9 p-0 mt-2 d-flex border border-info">
                 <button type="button" class="btn w-100" data-bs-toggle="modal" data-bs-target="#exampleModal">
                     Share art works
@@ -48,115 +50,83 @@ include('../User/Components/UserMetaData.php');
                         <div class="modal-dialog modal-dialog-centered">
                             <div class="modal-content  bg-light">
                                 <form method="post" action="../User/Handler/upload_picture.php" enctype="multipart/form-data">
-                                <div class="modal-body d-flex flex-column">
-                                    <div class="d-flex justify-content-between mb-2 p-0 align-items-center">
-                                        <p class="m-0 col-4">Create a post</p>
-                                    </div>
-                                    <div>
-                                        <textarea name="post_content" class="bg-white w-100 shadow-sm p-1 border-0 rounded" rows="2" placeholder="Add some caption for this masterpiece, <?php echo $_SESSION['display_name'] ?>... "></textarea>
-                                        <div id="img-container" class="w-100 my-4 d-flex justify-content-center d-none" style="height: 30vh;">
-                                            <img id="img-preview" style="object-fit: cover; object-position: center center;"/>
+                                    <div class="modal-body d-flex flex-column">
+                                        <div class="d-flex justify-content-between mb-2 p-0 align-items-center">
+                                            <p class="m-0 col-4">Create a post</p>
                                         </div>
+                                        <div>
+                                            <textarea name="post_content" class="bg-white w-100 shadow-sm p-1 border-0 rounded" rows="2" placeholder="Add some caption for this masterpiece, <?php echo $_SESSION['display_name'] ?>... "></textarea>
+                                            <div id="img-container" class="w-100 my-4 d-flex justify-content-center d-none" style="height: 30vh;">
+                                                <img id="img-preview" style="object-fit: cover; object-position: center center;" />
+                                            </div>
                                             <label for="art_photo" class="btn primary-color w-100 text-white">Upload Image</label>
                                             <input hidden name="art_photo" id="art_photo" type="file" accept="image/*">
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="modal-footer p-1">
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                    <button type="submit" name="submit_post" class="btn btn-primary">Post</button>
-                                </div>
+                                    <div class="modal-footer p-1">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <button type="submit" name="submit_post" class="btn btn-primary">Post</button>
+                                    </div>
                                 </form>
                             </div>
                         </div>
                     </div>
                 </form>
             </div>
-
-            <div data-post-id="' . $row['post_id'] . '" class="user_post rounded-2 shadow-sm border my-2 p-0 col-12 col-lg-9 d-flex flex-column overflow-hidden container flex-shrink-0">
-        <div id="PostCard_Header" class="bg-white m-0 d-flex justify-content-between p-2">
-            <p class="m-0 primary-fs" > Anonymous Poster </p>
-            <p class="m-0 primary-fs">' . $row['post_date'] . '</p>
+            <div id="post_container" class="d-flex flex-column w-100"></div>
         </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="https://cdn-script.com/ajax/libs/jquery/3.7.1/jquery.js"></script>
+        <script src="../Admin/Function/ContainerContentChanger.js"></script>
+        <script src="../Admin/Function/FetchUsers.js"></script>
+        <script src="../Admin/Function/ViewUserProfile.js"></script>
+        <script src="../User/Function/ArtGal_Retrieve_Post.js"></script>
 
-        <div id="PostCard_Body" style="background-color: ' . $postColor . '">
-            <p class="text-start m-2 mb-0" style="font-size:12px; color:'. $textColor .'">'. $chanel .'</p>
-            <div class="p-5">
-                
-                <p class="text-center mb-5 mt-3" style="color: ' . $textColor . '">' . nl2br($row['post_content']) . '</p>
-            </div>
-        </div>
-        <div id="PostCard_ActionBar" class="rounded bg-white d-flex justify-content-between p-2">';
+        <script>
+            $('#userDashboard').on('click', () => {
+                window.location.href = "../User/UserDashboard.php"
+            })
 
-        if (checkUserLikePost($row['post_id'], $account_id)) {
-            $htmlContent .= 
-            '<p style="cursor: default" class="disabled-like primary-fs m-0 like_post" data-post-id="' . $row['post_id'] .  '">
-                Likes <span class="primary-fs rounded text-white poppins-medium primary-color p-1" style="height:10px; width:10px;">' . getPostLike($row['post_id']) .  '</span>
-            </p>';
-        } else {
-            $htmlContent .= 
-            '<p style="cursor: pointer" class="m-0 like_post primary-fs" data-post-id="' . $row['post_id'] . '">
-                Likes <span class="primary-fs rounded text-white poppins-medium primary-color p-1" style="height:10px; width:10px;">' . getPostLike($row['post_id']) .  '</span>
-            </p>';
-        }
-
-        $htmlContent .= 
-            '<p id="comment_post" style="cursor: pointer" class="primary-fs m-0 comment_post" data-bs-toggle="modal" data-bs-target="#commentSectionModal-id-' . $row['post_id'] . '"> Comment 
-            <span class="rounded primary-fs text-white poppins-medium primary-color p-1" style="height:10px; width:10px;">' . (getPostCommentCount($row['post_id']) + getPostReplyCount($row['post_id'])) . '</p>
-
-        </div> 
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdn-script.com/ajax/libs/jquery/3.7.1/jquery.js"></script>
-    <script src="../Admin/Function/ContainerContentChanger.js"></script>
-    <script src="../Admin/Function/FetchUsers.js"></script>
-    <script src="../Admin/Function/ViewUserProfile.js"></script>
-
-    <script>
-        $('#userDashboard').on('click', ()=>{
-            window.location.href = "../User/UserDashboard.php"
-        })
-
-        $('#art_photo').on('change', function() {
-            var file = this.files[0]; //reference to the parent then get the first files value
-            if (file) {
-                var reader = new FileReader(); //Object for reading Files, makes file output as pic
-                $('#img-container').removeClass('d-none').addClass('d-block')
-                console.log(file)
-                reader.onload = function(e) { 
-                    $('#img-preview').attr('src', e.target.result);
-                }
-                reader.readAsDataURL(file)
-            }
-        })
-
-
-        $('#upload_photo_form').on('submit', function(e){
-            e.preventDefault();
-
-            let file = new FormData(this)
-            let img = file.get("art-photo")
-            let content = $('textarea[name="post_content"]').val();
-            
-            console.log(content)
-            if (!img || img.size === 0) {
-                alert("ADD IMAGE BRO")
-                return;
-            }
-
-            $.ajax({
-                url: '../User/Handler/upload_picture.php',
-                type: 'POST',
-                data: {
-                    file: file,
-                    content: content
-                },
-                contentType: false,
-                processData: false,
-                success: function() {
-                    console.log('succ')
+            $('#art_photo').on('change', function() {
+                var file = this.files[0]; //reference to the parent then get the first files value
+                if (file) {
+                    var reader = new FileReader(); //Object for reading Files, makes file output as pic
+                    $('#img-container').removeClass('d-none').addClass('d-block')
+                    console.log(file)
+                    reader.onload = function(e) {
+                        $('#img-preview').attr('src', e.target.result);
+                    }
+                    reader.readAsDataURL(file)
                 }
             })
-        })
-    </script>
+
+
+            $('#upload_photo_form').on('submit', function(e) {
+                e.preventDefault();
+
+                let file = new FormData(this)
+                let img = file.get("art_photo")
+                let content = $('textarea[name="post_content"]').val();
+
+                if (!img || img.size === 0) {
+                    alert("ADD IMAGE BRO")
+                    return;
+                }
+
+                file.append("content", content)
+
+                $.ajax({
+                    url: '../User/Handler/upload_picture.php',
+                    type: 'POST',
+                    data: file,
+                    contentType: false,
+                    processData: false,
+                    success: function() {
+                        window.location.reload()
+                    }
+                })
+            })
+        </script>
 </body>
+
 </html>
