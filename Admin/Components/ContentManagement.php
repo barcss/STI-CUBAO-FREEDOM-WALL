a1f4fd746f598564b178862608fc9e54be23897f
John Paul Barcinilla <barcinillajohnpaul106@gmail.com>
2025-08-23 19:07:10 +0800

    Include id and class to identify it within contentchangerfunction

--- Admin/Components/ContentManagement.php
+++ Admin/Components/ContentManagement.php
@@ -1,4 +1,6 @@
-<div id="response" class="w-100  m-0 p-0 d-flex justify-content-center flex-column text-center align-items-center"></div>
+<div id="content_management" class="content">
+<div id="response" class="w-100 m-0 p-0 d-flex justify-content-center flex-column text-center align-items-center"></div>
 <div id="loading" style="display: none; text-align: center;" class="d-flex justify-content-center ">
     <p>Loading more post</p>
+</div>
 </div>
