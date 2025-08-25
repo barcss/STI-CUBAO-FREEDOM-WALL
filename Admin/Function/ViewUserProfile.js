8b7747a8a73e404372e9ae229b5c4183e0e7375d
John Paul Barcinilla <barcinillajohnpaul106@gmail.com>
2025-08-25 22:30:39 +0800

    include total value for like comment and message

--- Admin/Function/ViewUserProfile.js
+++ Admin/Function/ViewUserProfile.js
@@ -1,5 +1,8 @@
 $('#users_container').on('click', '#profile', function() {
     $user_id = $(this).closest('.user-data').data('user-id')
+    $('#user_profile_record').removeClass('d-none').addClass('d-flex')
+    $('#user_mute').removeClass('d-block').addClass('d-none');
+
     $.ajax({
         url: '../Admin/Handler/RetrieveUserProfileInfo.php',
         dataType: 'Json',
@@ -12,14 +15,13 @@ $('#users_container').on('click', '#profile', function() {
             $('#profile_full_name').html(response.user_fullname)
             $('#profile_display_name').html(response.user_display_name)
             $('#profile_total_post span').html(response.user_total_post)
-            $('#public_total_message').html(response.user_total_chat)
+            $('#profile_total_comment span').html(response.user_total_comment)
+            $('#profile_total_message span').html(response.user_total_chat)
+            $('#profile_total_like span').html(response.user_total_like)
         },
         error: function(xhr){
             console.log('stats ' + xhr.status)
-            console.log('a ' + xhr.responseText)
-
             console.log('failed')
-
         }
     })
 })
